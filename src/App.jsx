c5b5b184d56f13400443b5cc0e57bc649b9dc715
AmirHosseinZameni <amirhossein.zameni@gmail.com>
2026-01-31 04:40:22 +0330

    feat(form): add controlled inputs and submit validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import Button from "./components/Button";
 import GoogleIcon from "./components/GoogleIcon";
 import FacebookIcon from "./components/FacebookIcon";
 import AppleIcon from "./components/AppleIcon";
+import { useState } from "react";
 
 
 function App() {
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+  function handleSubmit(e){
+    e.preventDefault();
+    if(!userName || !password) return
+     console.log("Submitted!", { userName, password });
+  }
   return (
     <div className="bg-[url('/assets/bg.png')]  w-screen h-screen bg-cover bg-center flex flex-row justify-end ">
       <form
-        action=""
+        onSubmit={handleSubmit}
         className="w-134 h-185 bg-[#f9f9f9] mt-auto mb-auto mr-29 rounded-[40px] pt-11 pr-11 pl-11 pb-20"
       >
         <div className="flex flex-row justify-between">
@@ -59,11 +67,18 @@ function App() {
           label="Enter your username or email address"
           className="flex flex-col gap-3 mt-13"
           placeHolder="Username or email address"
+          type="text"
+          value={userName}
+          onChange={setUserName}
+
         />
         <FormInput
           label="Enter your Password"
           className="flex flex-col gap-3 mt-9.5"
           placeHolder="Password"
+          type="password"
+          value={password}
+          onChange={setPassword}
         />
         <div className="flex flex-col items-end gap-11 mt-3">
           <a href="#" className="text-[#4285F4]">
@@ -71,7 +86,7 @@ function App() {
           </a>
           <input
             type="submit"
-            className="font-[Poppins] font-medium text-[16px] bg-[#779341] text-white w-59 h-14 rounded-[10px]"
+            className="font-[Poppins] font-medium text-[16px] bg-[#779341] text-white w-59 h-14 rounded-[10px] cursor-pointer"
             value="Sign in"
           />
         </div>
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,20 @@
-export default function FormInput({ label, className, placeHolder }) {
+export default function FormInput({
+  label,
+  className,
+  placeHolder,
+  type,
+  value,
+  onChange,
+}) {
   return (
     <div className={className}>
       <label className="font-[Poppins] font-regular text-[16px]">{label}</label>
       <input
-        type="text"
+        type={type}
         className="font-[Poppins] font-light text-[14px] border rounded-[9px] border-[#4285F4] pl-6.25 pt-4.75 pb-4.75"
         placeholder={placeHolder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
       />
     </div>
   );
